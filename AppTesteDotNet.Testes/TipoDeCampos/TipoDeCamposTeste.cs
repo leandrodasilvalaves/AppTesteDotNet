9062c8d25321390571aa88edbae856cf09d3dce2
Leandro da Silva Alves <leandro.silva.alves86@gmail.com>
2018-01-16 12:22:01 -0200

    Teste para tipos de campos

---
+++ AppTesteDotNet.Testes/TipoDeCampos/TipoDeCamposTeste.cs
@@ -0,0 +1,62 @@
+﻿using AppTesteDotNet.TipoDeCampos;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AppTesteDotNet.Testes.TipoDeCampos
+{
+    [TestClass]
+    public class TipoDeCamposTeste
+    {
+        [TestMethod]
+        public void DeveraRetornarUmElementoHTML_doTipoSelect()
+        {
+            string[] options = new string[3] {"Option1", "Option2", "Option3"};            
+            ITipoDeCampo campo = new SelectOptionCampo(options);
+
+            string actual = "<select><option value=''>Option1</option><option value=''>Option2</option><option value=''>Option3</option></select>";
+            string expected = campo.Renderizar();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DeveraRetornarUmElementoHtml_doTipoCheckBox()
+        {
+            ITipoDeCampo campo = new CheckBoxCampo("Option1");
+            string actual = "<input type='checkbox' name='' value=''> Option1";
+            string expected = campo.Renderizar();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DeveraRetornarUmElementoHtml_doTipoText()
+        {
+            ITipoDeCampo campo = new TextCampo();
+            string actual = "<input type='text' name='' value=''>";
+            string expected = campo.Renderizar();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DeveraRetornarUmElementoHtml_doTipoTextArea_SemInformarNumeroDeRows()
+        {
+            ITipoDeCampo campo = new TextAreaCampo();
+            string actual = "<textarea rows='5' cols=''></textarea>";
+            string expected = campo.Renderizar();
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void DeveraRetornarUmElementoHtml_doTipoTextArea_Com_10rows()
+        {
+            ITipoDeCampo campo = new TextAreaCampo(10);
+            string actual = "<textarea rows='10' cols=''></textarea>";
+            string expected = campo.Renderizar();
+
+            Assert.AreEqual(actual, expected);
+        }
+    }
+
+}
