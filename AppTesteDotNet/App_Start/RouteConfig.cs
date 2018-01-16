4c7295145949325356aee2024d58ba448082c8f0
Leandro da Silva Alves <leandro.silva.alves86@gmail.com>
2018-01-16 12:48:58 -0200

    Area administrativa inserida no projeto

--- AppTesteDotNet/App_Start/RouteConfig.cs
+++ AppTesteDotNet/App_Start/RouteConfig.cs
@@ -17,6 +17,7 @@ namespace AppTesteDotNet
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                , namespaces: new[] { "AppTesteDotNet.Controllers" }
             );
         }
     }
