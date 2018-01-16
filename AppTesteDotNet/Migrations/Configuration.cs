0b9816543b02c680d329c7884981feb4603f219f
Leandro da Silva Alves <leandro.silva.alves86@gmail.com>
2018-01-16 09:26:13 -0200

    Migrations habilitado

---
+++ AppTesteDotNet/Migrations/Configuration.cs
@@ -0,0 +1,72 @@
+﻿namespace AppTesteDotNet.Migrations
+{
+    using AppTesteDotNet.Models.Context;
+    using AppTesteDotNet.Models.Entities;
+    using System.Collections.Generic;
+    using System.Data.Entity.Migrations;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<AppContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(AppContext context)
+        {
+            var categoria = new Categoria()
+            {
+                Descricao = "Veículos e Acessórios",
+                Slug = "veiculos-acessorios",
+                SubCategorias = new List<SubCategoria>() {
+                    new SubCategoria(){
+                        Descricao ="Som",
+                        Slug ="som",
+                        Campos = new List<Campo>
+                        {
+                            new Campo{
+                                Ordem =1,
+                                Descricao ="Campo1",
+                                Tipo ="checkbox",
+                                Lista = new List<Lista>
+                                {
+                                    new Lista{Descricao="Opção1"},
+                                    new Lista{Descricao="Opção2" },
+                                    new Lista{Descricao="Opção3"}
+                                }
+                            },
+                            new Campo
+                            {
+                                Ordem=2,
+                                Descricao="Campo2",
+                                Tipo="select",
+                                Lista = new List<Lista>
+                                {
+                                    new Lista{Descricao="Opção1"},
+                                    new Lista{Descricao="Opção2" },
+                                    new Lista{Descricao="Opção3"}
+                                }
+                            },
+                            new Campo
+                            {
+                                Ordem=3,
+                                Descricao="Campo3",
+                                Tipo="Text"
+                            },
+                            new Campo
+                            {
+                                Ordem=4,
+                                Descricao="Campo4",
+                                Tipo="Textarea"
+                            }
+                        }
+                    }
+                }
+
+            };
+
+            context.Categorias.AddOrUpdate(categoria);
+            context.SaveChanges();
+        }
+    }
+}
